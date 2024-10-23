159de2e6c7a35e66cef00f5e1d1001a0a68402f1
magisterblaman <hannes.blaman@lbs.se>
2024-10-23 12:32:25 +0200

    Add project files.

---
+++ SY22_PRRPRR02_Övningsprov02_Genomgång/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY22_PRRPRR02_Övningsprov02_Genomgång {
+	internal class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
